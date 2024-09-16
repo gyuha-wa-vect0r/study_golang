c75b5f0b8eaa4474dd9b7679936551de5747c6f4
GYUHA <leejune30@chungbuk.ac.kr>
2024-09-16 19:00:44 +0900

    History to 19:01, 16SEP24

--- 16SEP24/ex4.1.go
+++ 16SEP24/ex4.1.go
@@ -6,6 +6,7 @@ func main() {
 	var a int = 10 // 생각보다 변수 선언법이 꽤나 직관적이네요
 	//  변수선언할래요 변수명은이렇구요 타입은이래요 초기값은얼마가들어가요 순
 	var msg string = "Hello Variable"
+	//
 
 	a = 20
 	msg = "Good Morning"
---
+++ 16SEP24/ex4.2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var minimumWage int = 10 // 변수 선언과 초기화요
+	var workingHour int = 20
+
+	var income int = minimumWage * workingHour // 변수 두개 곱한 값 저장이요
+
+	fmt.Println(minimumWage, workingHour, income) // 다 출력이요
+
+}
